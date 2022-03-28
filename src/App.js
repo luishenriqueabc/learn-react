a0a214f82b97672c55b7d2b4db6e2df3b05605b7
Luis Henrique <thais.liana2011@gmail.com>
2022-03-28 17:10:32 -0300

    Alteraçao do app.css e app.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello Word.
+          Meu primeiro projeto React.
         </p>
         <a
           className="App-link"
