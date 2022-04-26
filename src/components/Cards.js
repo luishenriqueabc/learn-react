19df57780cec622496d23e1ff5ac9d245bd6c57e
luishenriqueabc <thais.liana2011@gmail.com>
2022-04-25 22:15:02 -0300

    asda

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,19 +1,20 @@
 import './Cards.css';
+import SP from '../assets/SP.jpg';
 
 
 const Cards = () =>{
     return(
         <div className="cardsp">
-            <div className="cardispnfo">
-                <img src="SP."/>
+            <div className="cardspinfo ">
+      
+            </div>
                 <div className="SPletters">
                 <h3>São Paulo</h3>
                 <div className="text">
-                               <p>orem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja de tipos e os embaralhou para fazer um livro de modelos de tipos. Lorem Ipsum sobreviveu não só a cinco séculos, como também ao salto para a editoração eletrônica, permanecendo essencialmente inalterado.</p>
+                               <p>Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja de tipos e os embaralhou para fazer um livro de modelos de tipos. Lorem Ipsum sobreviveu não só a cinco séculos, como também ao salto para a editoração eletrônica, permanecendo essencialmente inalterado.</p>
                 </div>
                 </div>
             </div>
-        </div>
     )
 };
 export default Cards;
